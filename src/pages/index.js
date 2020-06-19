a20292865e52bc169025176a5af09e8c6fec3be8
Akshat Khatri <akshatster@gmail.com>
2020-06-19 20:46:06 +0100

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Layout from '../components/Layout'
-import { ExampleButton } from '../components/button'
+import { ExampleButton } from '../components/Button'
 
 function Home() {
   return (
